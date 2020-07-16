c0df076f27a9e16bda94d21386c35623af2bf0a2
Wataru Oguchi <wataru.oguchi.0to1@gmail.com>
2020-07-16 15:44:11 -0700

    Amplify API, Auth, Storage

--- src/App.js
+++ src/App.js
@@ -1,26 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { HashRouter, Switch, Route } from "react-router-dom";
+import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
+import { css } from "emotion";
+import { API, Storage } from "aws-amplify";
+import { listPosts } from "./graphql/queries";
 
-function App() {
+import Posts from "./Posts";
+import Post from "./Post";
+import Header from "./Header";
+import CreatePost from "./CreatePost";
+import Button from "./Button";
+
+function Router() {
+  /* create a couple of pieces of initial state */
+  const [showOverlay, updateOverlayVisibility] = useState(false);
+  const [posts, updatePosts] = useState([]);
+
+  /* fetch posts when component loads */
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+  async function fetchPosts() {
+    /* query the API, ask for 100 items */
+    let postData = await API.graphql({
+      query: listPosts,
+      variables: { limit: 100 },
+    });
+    let postsArray = postData.data.listPosts.items;
+    /* map over the image keys in the posts array, get signed image URLs for each image */
+    postsArray = await Promise.all(
+      postsArray.map(async (post) => {
+        const imageKey = await Storage.get(post.image);
+        post.image = imageKey;
+        return post;
+      })
+    );
+    /* update the posts array in the local state */
+    setPostState(postsArray);
+  }
+  async function setPostState(postsArray) {
+    updatePosts(postsArray);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <HashRouter>
+        <div className={contentStyle}>
+          <Header />
+          <hr className={dividerStyle} />
+          <Button
+            title="New Post"
+            onClick={() => updateOverlayVisibility(true)}
+          />
+          <Switch>
+            <Route exact path="/">
+              <Posts posts={posts} />
+            </Route>
+            <Route path="/post/:id">
+              <Post />
+            </Route>
+          </Switch>
+        </div>
+        <AmplifySignOut />
+      </HashRouter>
+      {showOverlay && (
+        <CreatePost
+          updateOverlayVisibility={updateOverlayVisibility}
+          updatePosts={setPostState}
+          posts={posts}
+        />
+      )}
+    </>
   );
 }
 
-export default App;
+const dividerStyle = css`
+  margin-top: 15px;
+`;
+
+const contentStyle = css`
+  min-height: calc(100vh - 45px);
+  padding: 0px 40px;
+`;
+
+export default withAuthenticator(Router);
